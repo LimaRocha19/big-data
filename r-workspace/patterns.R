7c9e7b178ac21d25d32b4a46132f6ff37dd3cdd0
Isaías Lima <isaiahlima18@gmail.com>
2020-05-12 15:19:18 -0300

    Implementação concluída, faltam os dados finais mas a lógica se mantem

--- r-workspace/patterns.R
+++ r-workspace/patterns.R
@@ -7,8 +7,164 @@
 
 # Agora, o foco é comparar diferentes dias e observar graficamente os padrões
 
-data <- smoothed_data('2020-05-05', '2020-05-06', 0.3)
+# Dias da semana (weekdays) - normais, sem feriado
 
-ggplot(data = data) + 
-  geom_line(mapping = aes(x = stamps, y = watts)) + 
-  geom_line(mapping = aes(x = stamps, y = smoothed), color = 'red')
+april_16 <- smoothed_data('2020-04-16', '2020-04-17', 0.3)
+april_17 <- smoothed_data('2020-04-17', '2020-04-18', 0.3)
+april_20 <- smoothed_data('2020-04-20', '2020-04-21', 0.3)
+april_22 <- smoothed_data('2020-04-22', '2020-04-23', 0.3)
+april_23 <- smoothed_data('2020-04-23', '2020-04-24', 0.3)
+april_24 <- smoothed_data('2020-04-24', '2020-04-25', 0.3)
+april_27 <- smoothed_data('2020-04-27', '2020-04-28', 0.3)
+april_28 <- smoothed_data('2020-04-28', '2020-04-29', 0.3)
+april_29 <- smoothed_data('2020-04-29', '2020-04-30', 0.3)
+april_30 <- smoothed_data('2020-04-30', '2020-05-01', 0.3)
+
+may_04 <- smoothed_data('2020-05-04', '2020-05-05', 0.3)
+may_05 <- smoothed_data('2020-05-05', '2020-05-06', 0.3)
+may_06 <- smoothed_data('2020-05-06', '2020-05-07', 0.3)
+may_07 <- smoothed_data('2020-05-07', '2020-05-08', 0.3)
+may_08 <- smoothed_data('2020-05-08', '2020-05-09', 0.3)
+
+# os dados dos últimos 5 dias serão implementados quando encerrarem as medições
+
+apr16 <- ggplot() + geom_line(data = april_16, mapping = aes(x = stamps, y = smoothed))
+apr17 <- ggplot() + geom_line(data = april_17, mapping = aes(x = stamps, y = smoothed))
+apr20 <- ggplot() + geom_line(data = april_20, mapping = aes(x = stamps, y = smoothed))
+apr22 <- ggplot() + geom_line(data = april_22, mapping = aes(x = stamps, y = smoothed))
+apr23 <- ggplot() + geom_line(data = april_23, mapping = aes(x = stamps, y = smoothed))
+apr24 <- ggplot() + geom_line(data = april_24, mapping = aes(x = stamps, y = smoothed))
+apr27 <- ggplot() + geom_line(data = april_27, mapping = aes(x = stamps, y = smoothed))
+apr28 <- ggplot() + geom_line(data = april_28, mapping = aes(x = stamps, y = smoothed))
+apr29 <- ggplot() + geom_line(data = april_29, mapping = aes(x = stamps, y = smoothed))
+apr30 <- ggplot() + geom_line(data = april_30, mapping = aes(x = stamps, y = smoothed))
+may04 <- ggplot() + geom_line(data = may_04, mapping = aes(x = stamps, y = smoothed))
+may05 <- ggplot() + geom_line(data = may_05, mapping = aes(x = stamps, y = smoothed))
+may06 <- ggplot() + geom_line(data = may_06, mapping = aes(x = stamps, y = smoothed))
+may07 <- ggplot() + geom_line(data = may_07, mapping = aes(x = stamps, y = smoothed))
+may08 <- ggplot() + geom_line(data = may_08, mapping = aes(x = stamps, y = smoothed))
+
+# segunda-feira
+grid.arrange(apr20, apr27, may04) 
+
+# terça-feira
+grid.arrange(apr28, may05)
+
+# quarta-feira
+grid.arrange(apr22, apr29, may06)
+
+# quinta-feira
+grid.arrange(apr16, apr23, apr30, may07)
+
+# sexta-feira
+grid.arrange(apr17, apr24, may08)
+  
+# Sábados, domingos e feriados  
+
+april_18 <- smoothed_data('2020-04-18', '2020-04-19', 0.3)
+april_19 <- smoothed_data('2020-04-19', '2020-04-20', 0.3)
+april_21 <- smoothed_data('2020-04-21', '2020-04-22', 0.3)
+april_25 <- smoothed_data('2020-04-25', '2020-04-26', 0.3)
+april_26 <- smoothed_data('2020-04-26', '2020-04-27', 0.3)
+may_01 <- smoothed_data('2020-05-01', '2020-05-02', 0.3)
+may_02 <- smoothed_data('2020-05-02', '2020-05-03', 0.3)
+may_03 <- smoothed_data('2020-05-03', '2020-05-04', 0.3)
+may_09 <- smoothed_data('2020-05-09', '2020-05-10', 0.3)
+may_10 <- smoothed_data('2020-05-10', '2020-05-11', 0.3)
+
+apr18 <- ggplot() + geom_line(data = april_18, mapping = aes(x = stamps, y = smoothed))
+apr19 <- ggplot() + geom_line(data = april_19, mapping = aes(x = stamps, y = smoothed))
+apr21 <- ggplot() + geom_line(data = april_21, mapping = aes(x = stamps, y = smoothed))
+apr25 <- ggplot() + geom_line(data = april_25, mapping = aes(x = stamps, y = smoothed))
+apr26 <- ggplot() + geom_line(data = april_26, mapping = aes(x = stamps, y = smoothed))
+may01 <- ggplot() + geom_line(data = may_01, mapping = aes(x = stamps, y = smoothed))
+may02 <- ggplot() + geom_line(data = may_02, mapping = aes(x = stamps, y = smoothed))
+may03 <- ggplot() + geom_line(data = may_03, mapping = aes(x = stamps, y = smoothed))
+may09 <- ggplot() + geom_line(data = may_09, mapping = aes(x = stamps, y = smoothed))
+may10 <- ggplot() + geom_line(data = may_10, mapping = aes(x = stamps, y = smoothed))
+
+# feriados
+grid.arrange(apr21, may01)
+
+# sábados
+grid.arrange(apr18, apr25, may02, may09)
+
+# domingos
+grid.arrange(apr19, apr26, may03, may10)
+
+# construção da tabela consolidada de padrões
+
+a16 <- transpose(april_16)
+a17 <- transpose(april_17)
+a18 <- transpose(april_18)
+a19 <- transpose(april_19)
+a20 <- transpose(april_20)
+a21 <- transpose(april_21)
+a22 <- transpose(april_22)
+a23 <- transpose(april_23)
+a24 <- transpose(april_24)
+a25 <- transpose(april_25)
+a26 <- transpose(april_26)
+a27 <- transpose(april_27)
+a28 <- transpose(april_28)
+a29 <- transpose(april_29)
+a30 <- transpose(april_30)
+m01 <- transpose(may_01)
+m02 <- transpose(may_02)
+m03 <- transpose(may_03)
+m04 <- transpose(may_04)
+m05 <- transpose(may_05)
+m06 <- transpose(may_06)
+m07 <- transpose(may_07)
+m08 <- transpose(may_08)
+m09 <- transpose(may_09)
+m10 <- transpose(may_10)
+
+patterns <- rbind(a16,
+                  a17,
+                  a18,
+                  a19,
+                  a20,
+                  a21,
+                  a22,
+                  a23,
+                  a24,
+                  a25,
+                  a26,
+                  a27,
+                  a28,
+                  a29,
+                  a30,
+                  m01,
+                  m02,
+                  m03,
+                  m04,
+                  m05,
+                  m06,
+                  m07,
+                  m08,
+                  m09,
+                  m10)
+
+# adicionando as datas observadas
+
+dates <- c('2020-04-16','2020-04-17','2020-04-18','2020-04-19','2020-04-20',
+           '2020-04-21','2020-04-22','2020-04-23','2020-04-24','2020-04-25',
+           '2020-04-26','2020-04-27','2020-04-28','2020-04-29','2020-04-30',
+           '2020-05-01','2020-05-02','2020-05-03','2020-05-04','2020-05-05',
+           '2020-05-06','2020-05-07','2020-05-08','2020-05-09','2020-05-10')
+
+patterns <- cbind(patterns, dates)
+patterns <- as_tibble(patterns)
+
+write.csv(patterns, 'patterns.csv')
+
+
+
+
+
+
+
+
+
+  
--- r-workspace/processing.R
+++ r-workspace/processing.R
@@ -12,6 +12,7 @@ library(TTR)
 library(tseries)
 library(mgcv)
 library(imputeTS)
+library(timeSeries)
 
 # conectando ao banco 
 db <- mongo('logs', url = 'mongodb+srv://lima:st10900152@cluster0-6zng5.gcp.mongodb.net/test?retryWrites=true&w=majority')
@@ -51,7 +52,7 @@ missing_stamps <- function (data) {
   begin <- data$stamps[1]
   endin <- data$stamps[n]
   
-  stamps <- seq(begin, endin, 1) # step de 4s é o mais observado nas análises
+  stamps <- seq(begin, endin, 1)
   
   times <- data.frame(stamps)
   # print(times)
@@ -62,3 +63,62 @@ missing_stamps <- function (data) {
   return(result)
 }
 
+# suaviazação das formas de onda
+
+smoothed_data <- function (from, to, f) {
+  consumption <- query_date(from, to)
+  stamps <- missing_stamps(consumption)
+  
+  # transformando os dados em uma série temporal
+  series <- ts(stamps$watts, start = 0, frequency = 24 * 3600)
+  smthed <- smoothLowess(as.timeSeries(series), f = f)
+  smthed <- ts(smthed$lowess, start = 0, frequency = 24 * 3600)
+  
+  result <- data.frame(stamps$stamps, as.numeric(series), as.numeric(smthed))
+  result <- as_tibble(result)
+  result <- result %>% 
+    mutate(stamps = anytime::anytime(stamps.stamps)) %>%
+    mutate(watts = as.numeric.series.) %>%
+    mutate(smoothed = as.numeric.smthed.) %>%
+    select(stamps, watts, smoothed)
+  return(result)
+}
+
+# como a ideia final é soltar uma base de dados com os padrões, serão amostrados
+# os consumos suavizados a cada 30 minutos
+
+half_hour_consumption <- function (data) {
+  
+  consumption <- data %>% mutate(stamps = as.numeric(paste0(as.integer(anytime::anytime(stamps))),''))
+  stamps <- seq(consumption$stamps[1], consumption$stamps[1] + 84600, 1800)
+  hours <- c('00:00:00','00:30:00','01:00:00','01:30:00',
+             '02:00:00','02:30:00','03:00:00','03:30:00',
+             '04:00:00','04:30:00','05:00:00','05:30:00',
+             '06:00:00','06:30:00','07:00:00','07:30:00',
+             '08:00:00','08:30:00','09:00:00','09:30:00',
+             '10:00:00','10:30:00','11:00:00','11:30:00',
+             '12:00:00','12:30:00','13:00:00','13:30:00',
+             '14:00:00','14:30:00','15:00:00','15:30:00',
+             '16:00:00','16:30:00','17:00:00','17:30:00',
+             '18:00:00','18:30:00','19:00:00','19:30:00',
+             '20:00:00','20:30:00','21:00:00','21:30:00',
+             '22:00:00','22:30:00','23:00:00','23:30:00')
+  
+  sampler <- as_tibble(data.frame(stamps, hours))
+  
+  result <- inner_join(consumption, sampler) %>% select(smoothed, hours) %>% distinct(hours, .keep_all= TRUE)
+  return(result)
+}
+
+# transposição para depois transformar tudo em uma tabela apenas
+
+transpose <- function(data) {
+  a <- half_hour_consumption(data)
+  # print(a)
+  cNames <- a$hours
+  a <- a %>% select(smoothed)
+  a <- t(a)
+  colnames(a) <- cNames
+  a <- as_tibble(a)
+  return(a)
+}
